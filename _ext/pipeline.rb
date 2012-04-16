cec96d39339daece8817265c3fbc9868bd7d0f24
Ivan Ivanov <ivan.ivanov@sap.com>
2012-04-17 00:08:43 +0300

    New pages for the forge website
    Added the important plugins section as well as the debugging forge page from the using forge section

--- _ext/pipeline.rb
+++ _ext/pipeline.rb
@@ -3,6 +3,7 @@ require 'docs'
 Awestruct::Extensions::Pipeline.new do
   extension Awestruct::Extensions::Posts.new( '/news' )
   extension Awestruct::Extensions::Docs::Index.new( '/docs/using', :docs_using) 
+  extension Awestruct::Extensions::Docs::Index.new( '/docs/important_plugins', :docs_important_plugins) 
   extension Awestruct::Extensions::Docs::Index.new( '/docs/plugin_development', :docs_plugin_development)
   # extension Awestruct::Extensions::Indexifier.new
 end
